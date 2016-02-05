fab0d32f45a9d26d6c816f86ed177cbe591d0d87
Jon Evans <jle@foraker.com>
2016-02-05 15:44:52 -0700

    Add question number/count to question serialization

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,6 +1,20 @@
 class QuestionSerializer < ActiveModel::Serializer
-  attributes :id, :content
+  attributes :id, :content, :number, :total_questions
 
   belongs_to :site
   has_many   :answers
+
+  def number
+    questions.pluck(:id).index(id) + 1
+  end
+
+  def total_questions
+    questions.count
+  end
+
+  private
+
+  def questions
+    @questions ||= site.questions
+  end
 end
